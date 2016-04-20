f3ddf5e72750f0d0f6f7a002768f12fe6abafb27
Tom Williamson <tom@dtssoftware.com>
2016-04-20 09:30:48 -0400

    Initial Commit

---
+++ Jackpot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jackpot
+//
+//  Created by Tom Williamson on 4/20/16.
+//  Copyright © 2016 Tom Williamson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jackpot/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Jackpot
+//
+//  Created by Tom Williamson on 4/20/16.
+//  Copyright © 2016 Tom Williamson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
