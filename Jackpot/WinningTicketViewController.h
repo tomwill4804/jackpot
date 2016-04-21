d2a09e45a5e9067ee215aee830619300b96b6e40
Tom Williamson <tom@dtssoftware.com>
2016-04-21 09:35:40 -0400

    new view controller

---
+++ Jackpot/WinningTicketViewController.h
@@ -0,0 +1,13 @@
+//
+//  WinningTicketViewController.h
+//  Jackpot
+//
+//  Created by Tom Williamson on 4/21/16.
+//  Copyright © 2016 Tom Williamson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WinningTicketViewController : UIViewController
+
+@end
